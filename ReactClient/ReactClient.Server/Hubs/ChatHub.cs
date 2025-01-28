24a3ee7f15e7b1b8970a907126643420ba8bb74a
Arthur Inda Rocha <arthur.rocha@mecomo.com>
2025-01-28 17:55:26 -0300

    Adicionar arquivos de projeto.

---
+++ ReactClient/ReactClient.Server/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.AspNetCore.SignalR;
+
+namespace ReactClient.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync() =>  await Clients.All.SendAsync("AllClientsNotification", $"{Context.ConnectionId} has joined the chat.");
+        public override async Task OnDisconnectedAsync(Exception? exception) => await Clients.All.SendAsync("AllClientsNotification", $"{Context.ConnectionId} has just left the chat.");
+        
+    }
+}
